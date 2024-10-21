3a1aa3eb396b9c6f83b568c6a4db85ada981ae07
mohamedfawas <fawastmh@gmail.com>
2024-10-22 05:03:35 +0530

    code completed

--- additional_problems/probs_review/top_k_frequent_elements.go
+++ additional_problems/probs_review/top_k_frequent_elements.go
@@ -2,59 +2,35 @@ package probsreview
 
 import "sort"
 
-// func topKFrequent(nums []int, k int) []int {
-// 	// Step 1: Create a map to store the frequency of each number
-// 	// The key is the number and the value is how many times it appears
-// 	freqMap := make(map[int]int)
-
-// 	// Count the frequency of each number in the array
-// 	for _, num := range nums {
-// 		freqMap[num]++
-// 	}
-
-// 	// Step 2: Create a slice to store unique numbers
-// 	// We'll sort this slice based on frequencies
-// 	uniqueNums := make([]int, 0, len(freqMap))
-// 	for num := range freqMap {
-// 		uniqueNums = append(uniqueNums, num)
-// 	}
-
-// 	// Step 3: Sort the unique numbers based on their frequencies
-// 	// We use sort.Slice with a custom less function
-// 	// This will sort in descending order (higher frequencies first)
-// 	sort.Slice(uniqueNums, func(i, j int) bool {
-// 		// If frequencies are equal, compare the numbers to ensure stable sort
-// 		if freqMap[uniqueNums[i]] == freqMap[uniqueNums[j]] {
-// 			return uniqueNums[i] < uniqueNums[j]
-// 		}
-
-// 		return freqMap[uniqueNums[i]] > freqMap[uniqueNums[j]]
-// 	})
-
-// 	// Step 4: Return the first k elements
-// 	return uniqueNums[:k]
-// }
-
 func topKFrequent(nums []int, k int) []int {
+	// Step 1: Create a map to store the frequency of each number
+	// The key is the number and the value is how many times it appears
 	freqMap := make(map[int]int)
 
+	// Count the frequency of each number in the array
 	for _, num := range nums {
 		freqMap[num]++
 	}
 
+	// Step 2: Create a slice to store unique numbers
+	// We'll sort this slice based on frequencies
 	uniqueNums := make([]int, 0, len(freqMap))
-
 	for num := range freqMap {
 		uniqueNums = append(uniqueNums, num)
 	}
 
+	// Step 3: Sort the unique numbers based on their frequencies
+	// We use sort.Slice with a custom less function
+	// This will sort in descending order (higher frequencies first)
 	sort.Slice(uniqueNums, func(i, j int) bool {
+		// If frequencies are equal, compare the numbers to ensure stable sort
 		if freqMap[uniqueNums[i]] == freqMap[uniqueNums[j]] {
-			return freqMap[uniqueNums[i]] < freqMap[uniqueNums[j]]
+			return uniqueNums[i] < uniqueNums[j]
 		}
 
 		return freqMap[uniqueNums[i]] > freqMap[uniqueNums[j]]
 	})
 
+	// Step 4: Return the first k elements
 	return uniqueNums[:k]
 }
