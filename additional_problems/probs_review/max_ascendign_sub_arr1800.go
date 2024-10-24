1e9be75e163d4a202d1910f3d3360eefa5d93122
mohamedfawas <fawastmh@gmail.com>
2024-10-24 09:47:03 +0530

    add comments

--- additional_problems/probs_review/max_ascendign_sub_arr1800.go
+++ additional_problems/probs_review/max_ascendign_sub_arr1800.go
@@ -1,5 +1,17 @@
 package probsreview
 
+/*
+if len(arr)==0, return 0
+if len(arr)==1, return first element
+iterate from second element
+	if arr[i]>arr[i-1]
+		currSum += arr[i]
+			if currSum>maxSum, update
+	else
+		currSum = arr[i]
+
+return maxSum
+*/
 func maxAscendingSum(nums []int) int {
 	// If the array is empty
 	if len(nums) == 0 {
