f67fd1836454c57559701c48d80b25de228154ae
mohamedfawas <fawastmh@gmail.com>
2024-11-03 13:47:07 +0530

    insertion sort code covered

--- sorting/main.go
+++ sorting/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	bubblesort "sorting/bubble_sorting"
-	selectionsorting "sorting/selection_sorting"
+	insertionsort "sorting/insertion_sort"
 )
 
 func main() {
 	nums := []int{5, 6, 4, 7, 3, 44, 77, 23, 9}
 	// sort
-	selectionsorting.SelectionSort(nums)
-	fmt.Printf("After selection sorting : %v", nums)
-
-	nums2 := []int{5, 6, 4, 7, 3, 44, 77, 23, 9, 34}
-	bubblesort.BubbleSort(nums2)
-	fmt.Printf("after bubble sort is : %v", nums2)
+	insertionsort.InsertionSort(nums)
+	fmt.Printf("After  sorting : %v", nums)
 }
