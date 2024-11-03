5cce5f750e3d982dfb38fe53b25471a438549293
mohamedfawas <fawastmh@gmail.com>
2024-11-03 16:50:24 +0530

    stack 3rd problem solution code added

---
+++ stack_and_queue/stack/leetcode/MaxNestDepthParentheses.go
@@ -0,0 +1,16 @@
+package stackleetcode
+
+func maxDepth(s string) int {
+	depth, maxdepth := 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			depth++
+		} else if s[i] == ')' {
+			depth--
+		}
+		if depth > maxdepth {
+			maxdepth = depth
+		}
+	}
+	return maxdepth
+}
